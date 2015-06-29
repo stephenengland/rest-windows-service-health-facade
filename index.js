2a8fdbb9357b5fe681a1a84d5f8c760546798d7e
Stephen England <thealah@gmail.com>
2015-06-28 22:52:51 -0500

    Added querying IIS websites

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
   nconf = require('nconf'),
+  path = require('path'),
   lazy = require("lazy"),
   childProcess = require('child_process'),
   app = express();
@@ -7,6 +8,12 @@ var express = require('express'),
 nconf.argv().env();
 nconf.file("./config.json");
 nconf.load();
+
+var badRequest = function (res, reason) {
+    reason = "The data you sent was invalid. " + (reason || "");
+    res.hasEnded = true;
+    res.status(500).send(reason).end();
+};
 
 var stripWindowsServiceCLILine = function (ioLine) {
   ioLine = ioLine.trim();
@@ -52,6 +59,112 @@ var getServices = function (filter, callback) {
     });
 };
 
+var stripIisSiteLine = function (ioLine) {
+  ioLine = ioLine.trim();
+  return ioLine;
+};
+
+var getIisWebsites = function (siteFilter, callback) {
+  var args = ['list', 'site', '/state:started', '/text:name'];
+  if (siteFilter) {
+    args.push('/name:' + siteFilter);
+  }
+  var child = childProcess.spawn(path.join(process.env.systemroot, 'system32', 'inetsrv', 'appcmd.exe'), args);
+  var errData = '';
+  var websites = [];
+  child.stderr.on('data', function (chunk){
+    errData += chunk;
+  });
+  child.on('exit', function () {
+    if (errData) {
+      console.log(errData);
+      return callback(errData, []);
+    }
+
+    callback(undefined, websites);
+  });
+
+  lazy(child.stdout)
+    .lines
+    .map(String)
+    .map(stripIisSiteLine)
+    .forEach(function(website){
+      websites.push(website);
+    });
+};
+
+app.get('/iis/info/:site', function (req, res) {
+  var host = server.address().address;
+  if (host === '::') {
+    host = "localhost";
+  }
+  var port = server.address().port;
+
+  res.json({
+    "description": "IIS Website is monitored by a REST API - https://github.com/thealah/rest-windows-service-health-facade",
+    "website": req.params.site,
+    "serverHost": host,
+    "serverPort": port,
+    "ui": {
+      "hide": ["serverHost", "serverPort"]
+    }
+  });
+  res.end();
+});
+
+app.get('/iis/:site', function (req, res) {
+  var siteFilter = req.params.site;
+  if (!siteFilter || /^[a-zA-Z0-9- ]+$/.test(siteFilter) == false) {
+    badRequest(res, "Invalid Site Name");
+    return;
+  }
+
+  getIisWebsites(siteFilter, function (err, websites) {
+    if (err) {
+      res.status(500).json({
+        "type": "Website",
+        "message": "Error reading IIS Websites",
+        "ui": {
+          "info": "/iis/info/" + encodeURIComponent(siteFilter)
+        }
+      });
+    }
+    else if (!websites.length) {
+      res.status(502).json({
+        "type": "Website",
+        "message": "Missing Website '" + siteFilter + "' or it is stopped",
+        "ui": {
+          "info": "/iis/info/" + encodeURIComponent(siteFilter)
+        }
+      });
+    }
+    else {
+      res.json({
+        "type": "Website",
+        "ui": {
+          "info": "/iis/info/" + encodeURIComponent(websites[0])
+        }
+      });
+    }
+    res.end();
+  });
+});
+
+app.get('/iis', function (req, res) {
+  getIisWebsites(undefined, function (err, websites) {
+    if (err) {
+      res.status(500);
+    }
+    else {
+      res.json({
+        "services": websites,
+        "message": "To use the healthcheck portion of the API, use the route: /iis/$IIS_SITE_NAME"
+      }); 
+    }
+    res.end();
+  });
+});
+
 app.get('/info/:service', function (req, res) {
   var host = server.address().address;
   if (host === '::') {
@@ -74,7 +187,7 @@ app.get('/info/:service', function (req, res) {
 app.get('/:service', function (req, res) {
   var serviceFilter = req.params.service;
   if (!serviceFilter) {
-    response.badRequest(res, "Invalid Service Name");
+    badRequest(res, "Invalid Service Name");
     return;
   }
 
